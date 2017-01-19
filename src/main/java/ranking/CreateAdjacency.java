7f09cdb628531e20c30c9e72cf4201827cccfb20
mozi22 <muazzamalii@hotmail.com>
2017-01-19 22:38:38 +0100

    app-config changed

---
+++ src/main/java/ranking/CreateAdjacency.java
@@ -0,0 +1,32 @@
+package ranking;
+
+public class CreateAdjacency {
+	
+	/* 
+	 * We reduce the values(objects) based on the keys(subjects)
+	 * 
+	 * ---------------------------
+	 * |  subject 	|	object	|
+	 * |			|			|
+	 * |	1		|	5		|
+	 * |	1		|	6		|
+	 * |	1		|	7		|
+	 * |	2		|	5		|
+	 * |	2		|	8		|
+	 * --------------------------
+
+	 * After reducing
+	 * 
+	 * 1 --> 5 , 6 , 7 
+	 * 2 --> 5 , 8
+	 */
+	public void performReduceOperation(){
+		
+	}
+	
+	
+	public void getNodes(){
+		
+	}
+
+}
